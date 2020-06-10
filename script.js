1a38be2361ee029ad1a3d7f58f3bb4a362de06c4
JordanBelmont <jordan.belmont@hotmail.com>
2020-06-10 20:53:13 +0100

    added array of answers to script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let answers = [
+   "Indubitably",
+   "Yes, at a price",
+   "I would bet your life on it",
+   "Oh why not, I'm feeling generous",
+   "By the Plateaus of Leng, it shall be so",
+   "I'll allow it",
+   "Ask nicer",
+   "Pointless questions relinquish pointless answers",
+   "You not knowing is half my fun",
+   "Limitless knowledge, and you waste it with this?",
+   "Some things are best left a mystery",
+   "I could tell you, but then I'd have to kill you",
+   "Not today, mortal",
+   "It will never come to pass",
+   "I think not",
+   "Only in your dreams",
+   "I wouldn't hold my breath",
+   "That's going to be a 'no'"
+];
